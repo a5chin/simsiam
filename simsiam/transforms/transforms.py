1507ca00208295ef1b1a2f53a077d8abb42d619e
a5chin <capricorn42551230@gmail.com>
2022-05-23 00:36:29 +0900

    create loss and transforms for simsiam

---
+++ simsiam/transforms/transforms.py
@@ -0,0 +1,39 @@
+import random
+from typing import List
+
+import torch
+from PIL import ImageFilter
+from torchvision import transforms
+
+
+class TwoCropsTransform:
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, base_transform):
+        self.base_transform = base_transform
+
+    def __call__(self, x) -> List[torch.Tensor]:
+        q = self.base_transform(x)
+        k = self.base_transform(x)
+        return [q, k]
+
+
+class GaussianBlur:
+    def __init__(self, sigma=[0.1, 2.0]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
+
+
+def get_transforms():
+    base_transform = transforms.Compose([
+        transforms.RandomResizedCrop(size=(512, 512), scale=(0.2, 1.)),
+        transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    return TwoCropsTransform(base_transform)
